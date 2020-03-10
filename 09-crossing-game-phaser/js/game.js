60de8897f7f2a2262ff2f1da70a7c6fc283e358e
Yuliia <julianna.smirnova@gmail.com>
2020-03-11 00:11:47 +0200

    feat: add bg to canvas

--- 09-crossing-game-phaser/js/game.js
+++ 09-crossing-game-phaser/js/game.js
@@ -1,5 +1,15 @@
 let gameScene = new Phaser.Scene('Game');
 
+gameScene.preload = function() {
+    this.load.image('background', 'assets/background.png');
+    this.load.image('player', 'assets/player.png');
+};
+
+gameScene.create = function() {
+    let bg = this.add.sprite(0, 0, 'background');
+    bg.setOrigin(0, 0);
+};
+
 let config = {
     type: Phaser.AUTO,
     width: 640,
