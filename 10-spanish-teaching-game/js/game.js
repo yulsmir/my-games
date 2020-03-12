4afeb1a00b8181bb611c5b56b6bf9de28da95bf1
Yuliia <julianna.smirnova@gmail.com>
2020-03-13 00:07:14 +0200

    refactor: remove old projects, add sounds to spanish game

--- 10-spanish-teaching-game/js/game.js
+++ 10-spanish-teaching-game/js/game.js
@@ -2,17 +2,33 @@
 let gameScene = new Phaser.Scene('Game');
 
 // some parameters for our scene
-gameScene.init = function() {}
+gameScene.init = function() {};
 
 // load asset files for our game
 gameScene.preload = function() {
+  this.load.image('background', 'assets/images/background-city.png');
+  this.load.image('background', 'assets/images/building.png');
+  this.load.image('background', 'assets/images/car.png');
+  this.load.image('background', 'assets/images/house.png');
+  this.load.image('background', 'assets/images/tree.png');
 
+  this.load.audio('treeAudio', 'assets/audio/arbol.mp3');
+  this.load.audio('carAudio', 'assets/audio/auto.mp3');
+  this.load.audio('houseAudio', 'assets/audio/car.mp3');
+  this.load.audio('buildingAudio', 'assets/audio/edificio.mp3');
+  this.load.audio('correct', 'assets/audio/correct.mp3');
+  this.load.audio('wrong', 'assets/audio/wrong.mp3');
 };
 
 // executed once, after assets were loaded
 gameScene.create = function() {
+  this.add.sprite(0, 0, 'background').setOrigin(0, 0);
 
-
+  let soundSample = this.sound.add('correct');
+  soundSample.play();
+  // soundSample.stop();
+  // soundSample.pause();
+  // soundSample.resume();
 };
 
 // our game's configuration
