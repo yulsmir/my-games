55ecd24297b0a9319f60c6f10d2bf0440fa563fa
Yuliia <julianna.smirnova@gmail.com>
2020-03-13 00:23:29 +0200

    feat: add images and sounds

--- 10-spanish-teaching-game/js/game.js
+++ 10-spanish-teaching-game/js/game.js
@@ -2,43 +2,80 @@
 let gameScene = new Phaser.Scene('Game');
 
 // some parameters for our scene
-gameScene.init = function() {};
+gameScene.init = function () {
+};
 
 // load asset files for our game
-gameScene.preload = function() {
-  this.load.image('background', 'assets/images/background-city.png');
-  this.load.image('background', 'assets/images/building.png');
-  this.load.image('background', 'assets/images/car.png');
-  this.load.image('background', 'assets/images/house.png');
-  this.load.image('background', 'assets/images/tree.png');
+gameScene.preload = function () {
+    this.load.image('background', 'assets/images/background-city.png');
+    this.load.image('building', 'assets/images/building.png');
+    this.load.image('car', 'assets/images/car.png');
+    this.load.image('house', 'assets/images/house.png');
+    this.load.image('tree', 'assets/images/tree.png');
 
-  this.load.audio('treeAudio', 'assets/audio/arbol.mp3');
-  this.load.audio('carAudio', 'assets/audio/auto.mp3');
-  this.load.audio('houseAudio', 'assets/audio/car.mp3');
-  this.load.audio('buildingAudio', 'assets/audio/edificio.mp3');
-  this.load.audio('correct', 'assets/audio/correct.mp3');
-  this.load.audio('wrong', 'assets/audio/wrong.mp3');
+    this.load.audio('treeAudio', 'assets/audio/arbol.mp3');
+    this.load.audio('carAudio', 'assets/audio/auto.mp3');
+    this.load.audio('houseAudio', 'assets/audio/car.mp3');
+    this.load.audio('buildingAudio', 'assets/audio/edificio.mp3');
+    this.load.audio('correct', 'assets/audio/correct.mp3');
+    this.load.audio('wrong', 'assets/audio/wrong.mp3');
 };
 
 // executed once, after assets were loaded
-gameScene.create = function() {
-  this.add.sprite(0, 0, 'background').setOrigin(0, 0);
+gameScene.create = function () {
+    this.add.sprite(0, 0, 'background').setOrigin(0, 0);
+    this.items = this.add.group([
+        {
+            key: 'building',
+            setXY: {
+                x: 100,
+                y: 240
+            }
+        },
+        {
+            key: 'house',
+            setXY: {
+                x: 240,
+                y: 280
+            },
+            setScale: {
+                x: 0.8,
+                y: 0.8
+            }
+        },
+      {
+        key: 'car',
+        setXY: {
+          x: 400,
+          y: 300
+        },
+        setScale: {
+          x: 0.8,
+          y: 0.8
+        }
+      },
+      {
+        key: 'tree',
+        setXY: {
+          x: 550,
+          y: 250
+        },
+      },
+    ]);
 
-  let soundSample = this.sound.add('correct');
-  soundSample.play();
-  // soundSample.stop();
-  // soundSample.pause();
-  // soundSample.resume();
+    // soundSample.stop();
+    // soundSample.pause();
+    // soundSample.resume();
 };
 
 // our game's configuration
 let config = {
-  type: Phaser.AUTO,
-  width: 640,
-  height: 360,
-  scene: gameScene,
-  title: 'Spanish Learning Game',
-  pixelArt: false,
+    type: Phaser.AUTO,
+    width: 640,
+    height: 360,
+    scene: gameScene,
+    title: 'Spanish Learning Game',
+    pixelArt: false,
 };
 
 // create the game, and pass it the configuration
