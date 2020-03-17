ef20cdef14a8aacdbcade3666316c81f0d008ce6
Yuliia <julianna.smirnova@gmail.com>
2020-03-17 16:35:32 +0200

    feat: add platform and ground

--- 12-platformer-phaser/js/main.js
+++ 12-platformer-phaser/js/main.js
@@ -2,45 +2,65 @@
 let gameScene = new Phaser.Scene('Game');
 
 // some parameters for our scene
-gameScene.init = function() {};
+gameScene.init = function () {
+};
 
 // load asset files for our game
-gameScene.preload = function() {
+gameScene.preload = function () {
 
-  // load images
-  this.load.image('ground', 'assets/images/ground.png');
-  this.load.image('platform', 'assets/images/platform.png');
-  this.load.image('block', 'assets/images/block.png');
-  this.load.image('goal', 'assets/images/gorilla3.png');
-  this.load.image('barrel', 'assets/images/barrel.png');
+    // load images
+    this.load.image('ground', 'assets/images/ground.png');
+    this.load.image('platform', 'assets/images/platform.png');
+    this.load.image('block', 'assets/images/block.png');
+    this.load.image('goal', 'assets/images/gorilla3.png');
+    this.load.image('barrel', 'assets/images/barrel.png');
 
-  // load spritesheets
-  this.load.spritesheet('player', 'assets/images/player_spritesheet.png', {
-    frameWidth: 28,
-    frameHeight: 30,
-    margin: 1,
-    spacing: 1
-  });
+    // load spritesheets
+    this.load.spritesheet('player', 'assets/images/player_spritesheet.png', {
+        frameWidth: 28,
+        frameHeight: 30,
+        margin: 1,
+        spacing: 1
+    });
 
-  this.load.spritesheet('fire', 'assets/images/fire_spritesheet.png', {
-    frameWidth: 20,
-    frameHeight: 21,
-    margin: 1,
-    spacing: 1
-  });
+    this.load.spritesheet('fire', 'assets/images/fire_spritesheet.png', {
+        frameWidth: 20,
+        frameHeight: 21,
+        margin: 1,
+        spacing: 1
+    });
 };
 
 // executed once, after assets were loaded
-gameScene.create = function() {};
+gameScene.create = function () {
+    let ground = this.add.sprite(180, 604, 'ground');
+    this.physics.add.existing(ground, true);
+
+
+    let platform = this.add.tileSprite(180, 500, 4 * 36, 1 * 30, 'block');
+
+
+    // ground.body.allowGravity = false;
+    // ground.body.immovable = true;
+    // let ground2 = this.physics.add.sprite(180, 200, 'ground');
+    // this.physics.add.collider(ground, ground2);
+};
 
 // our game's configuration
 let config = {
-  type: Phaser.AUTO,
-  width: 360,
-  height: 640,
-  scene: gameScene,
-  title: 'Monster Kong',
-  pixelArt: false
+    type: Phaser.AUTO,
+    width: 360,
+    height: 640,
+    scene: gameScene,
+    title: 'Monster Kong',
+    pixelArt: false,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y: 1000},
+            debug: true
+        }
+    }
 };
 
 // create the game, and pass it the configuration
